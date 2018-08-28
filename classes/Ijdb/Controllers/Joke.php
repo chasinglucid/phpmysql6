1dd7fab90a7fbc9f27687b8c4b54929b69623347
chasinglucid <35589621+chasinglucid@users.noreply.github.com>
2018-08-28 16:06:48 -0600

    Correct Error & Add User Checking for Security
    This contains an error. The Joke.php file generates a strange error regarding the authorid

--- classes/Ijdb/Controllers/Joke.php
+++ classes/Ijdb/Controllers/Joke.php
@@ -4,89 +4,101 @@ use \Ninja\DatabaseTable;
 use \Ninja\Authentication;
 
 class Joke {
-	private $authorsTable;
-	private $jokesTable;
+  private $authorsTable;
+  private $jokesTable;
 
-	public function __construct(DatabaseTable $jokesTable, DatabaseTable $authorsTable, Authentication $authentication) {
-		$this->jokesTable = $jokesTable;
-		$this->authorsTable = $authorsTable;
-		$this->authentication = $authentication;
-	}
+  public function __construct(DatabaseTable $jokesTable, DatabaseTable $authorsTable, Authentication $authentication) {
+    $this->jokesTable = $jokesTable;
+    $this->authorsTable = $authorsTable;
+    $this->authentication = $authentication;
+  }
 
-	public function list() {
-		$result = $this->jokesTable->findAll();
+  public function list() {
+    $result = $this->jokesTable->findAll();
 
-		$jokes = [];
-		foreach ($result as $joke) {
-			$author = $this->authorsTable->findById($joke['authorId']);
+    $jokes = [];
+    foreach ($result as $joke) {
+      $author = $this->authorsTable->findById($joke['authorid']);
 
-			$jokes[] = [
-				'id' => $joke['id'],
-				'joketext' => $joke['joketext'],
-				'jokedate' => $joke['jokedate'],
-				'name' => $author['name'],
-				'email' => $author['email']
-			];
+      $jokes[] = [
+        'id' => $joke['id'],
+        'joketext' => $joke['joketext'],
+        'jokedate' => $joke['jokedate'],
+        'name' => $author['name'],
+        'email' => $author['email'],
+        'authorid' => $author['id']
+      ];
 
-		}
+    }
 
 
-		$title = 'Joke list';
+    $title = 'Joke list';
 
-		$totalJokes = $this->jokesTable->total();
+    $totalJokes = $this->jokesTable->total();
 
-		ob_start();
+    $author = $this->authentication->getUser();
 
-		include  __DIR__ . '/../../templates/';
+    //		ob_start();
 
-		$output = ob_get_clean();
+    //		include  __DIR__ . '/../../templates/';
 
-		return ['template' => 'jokes.html.php', 
-				'title' => $title, 
-				'variables' => [
-						'totalJokes' => $totalJokes,
-						'jokes' => $jokes
-					]
-				];
-	}
+    //		$output = ob_get_clean();
 
-	public function home() {
-		$title = 'Internet Joke Database';
+    return ['template' => 'jokes.html.php', 
+            'title' => $title, 
+            'variables' => [
+              'totalJokes' => $totalJokes,
+              'jokes' => $jokes,
+              'userId' => $author['id'] ?? null
+            ]
+           ];
+  }
 
-		return ['template' => 'home.html.php', 'title' => $title];
-	}
+  public function home() {
+    $title = 'Internet Joke Database';
 
-	public function delete() {
-		$this->jokesTable->delete($_POST['id']);
+    return ['template' => 'home.html.php', 'title' => $title];
+  }
 
-		header('location: /joke/list'); 
-	}
+  public function delete() {
+    $author = $this->authentication->getUser();
+    $joke = $this->jokesTable->findById($_POST['id']);
+    if ($joke['authorId'] != $author['id']) {
+      return;
+    }
+    $this->jokesTable->delete($_POST['id']);
+    header('location: /joke/list'); 
+  }
 
-	public function saveEdit() {
-		$author = $this->authentication->getUser();
+  public function saveEdit() {
+    $author = $this->authentication->getUser();
 
-		$joke = $_POST['joke'];
-		$joke['jokedate'] = new \DateTime();
-		$joke['authorId'] = $author['id'];
+    if (isset($_GET['id'])) {
+      $joke = $this->jokesTable->findById($_GET['id']);
+      if ($joke['authorid'] != $author['id']) {
+        return;
+      }
+    }
+    $joke = $_POST['joke'];
+    $joke['jokedate'] = new \DateTime();
+    $joke['authorid'] = $author['id'];
+    $this->jokesTable->save($joke);
+    header('location: /joke/list'); 
+  }
 
-		$this->jokesTable->save($joke);
-		
-		header('location: /joke/list'); 
-	}
+  public function edit() {
+    $author = $this->authentication->getUser();
+    if (isset($_GET['id'])) {
+      $joke = $this->jokesTable->findById($_GET['id']);
+    }
+    $title = 'Edit joke';
+    return ['template' => 'editjoke.html.php',
+            'title' => $title,
+            'variables' => [
+              'joke' => $joke ?? null,
+              'userId' => $author['id'] ?? null
+            ]
+           ];
+  }
 
-	public function edit() {
-		if (isset($_GET['id'])) {
-			$joke = $this->jokesTable->findById($_GET['id']);
-		}
-
-		$title = 'Edit joke';
-
-		return ['template' => 'editjoke.html.php',
-				'title' => $title,
-				'variables' => [
-						'joke' => $joke ?? null
-					]
-				];
-	}
-	
 }
